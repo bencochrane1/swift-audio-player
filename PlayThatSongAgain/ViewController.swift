5a3515ad9cda3aae35a42bc02daef2879b7aecf2
Ben Cochrane <bencochrane1@me.com>
2015-04-13 07:38:26 +1000

    Initial Commit

---
+++ PlayThatSongAgain/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PlayThatSongAgain
+//
+//  Created by Ben Cochrane on 13/04/2015.
+//  Copyright (c) 2015 benCochrane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
